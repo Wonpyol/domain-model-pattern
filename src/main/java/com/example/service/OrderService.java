1ae9023c751b3abc2260533fc30c4c1208151587
wonpyol <wonpyol@gmail.com>
2025-04-09 10:59:01 +0900

    feat: b-commit

--- src/main/java/com/example/service/OrderService.java
+++ src/main/java/com/example/service/OrderService.java
@@ -31,6 +31,7 @@ public class OrderService {
         //배송정보
         Delivery delivery = Delivery.builder().address(member.getAddress()).build();
         //주문상품생성
+        //b-commit-2
         OrderItem orderItem = OrderItem.createOrderItem(item, item.getPrice(), count);
         //주문생성
         Order order = Order.createOrder(member, delivery, orderItem);
